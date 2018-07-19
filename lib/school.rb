810b0c9d07b890b74541fbd132cfb30593517ad6
Sharon Mun <sharonymun@gmail.com>
2018-07-19 15:45:34 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+  attr_accessor = :roster
+  
+  def initialize(school)
+    @school = school
+  end
+  
+  def school
+    @school
+  end
+  
+  def roster=(roster) #writer method
+    @roster = roster
+  end
+
+  :roster = {
+    :key => ["hash"]
+  }
+  #create value and keys
+  #value => keys
+  
+end
+
+#NEWWWWWW
+
+school = School.new
+school.school_properties = "Bayside High School" #writer method
+puts school.school_properties                    #reader method
+
+roster = School.new
+roster.school_properties = "grade levels"
